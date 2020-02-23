f4f8904e576f0277bc99a8fa433d7d7597222757
Legenda512 <47562414+Legenda512@users.noreply.github.com>
2020-02-23 18:59:37 +0700

    Работает интерфейс, сейчас дальше делаю отображение задачи в одном окне

---
+++ TMS_task_management_system/ApplicationViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Runtime.CompilerServices;
+
+namespace TMS_task_management_system
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        ApplicationContext db;
+        RelayCommand addCommand;
+        RelayCommand editCommand;
+        RelayCommand deleteCommand;
+        IEnumerable<Task> tasks;
+
+        public IEnumerable<Task> Tasks
+        {
+            get { return tasks; }
+            set
+            {
+                tasks = value;
+                OnPropertyChanged("Tasks");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            db = new ApplicationContext();
+            db.Tasks.Load();
+            Tasks = db.Tasks.Local.ToBindingList();
+        }
+        // команда добавления
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand((o) =>
+                  {
+                      TaskWindow phoneWindow = new TaskWindow(new Task());
+                      if (phoneWindow.ShowDialog() == true)
+                      {
+                          Task task = phoneWindow.Task;
+                          db.Tasks.Add(task);
+                          db.SaveChanges();
+                      }
+                  }));
+            }
+        }
+        // команда редактирования
+        public RelayCommand EditCommand
+        {
+            get
+            {
+                return editCommand ??
+                  (editCommand = new RelayCommand((selectedItem) =>
+                  {
+                      if (selectedItem == null) return;
+                      // получаем выделенный объект
+                      Task task = selectedItem as Task;
+
+                      Task vm = new Task()
+                      {
+                          Id_Task = task.Id_Task,
+                          Name_Task = task.Name_Task,
+                          Description_Task = task.Description_Task,
+                          Performers_Task = task.Performers_Task,
+                          Date_of_registration_Task = task.Date_of_registration_Task,
+                          Status_Task = task.Status_Task,
+                          Planned_time_Task = task.Planned_time_Task,
+                          Actual_time_Task = task.Actual_time_Task,
+                          Date_of_completion_Task = task.Date_of_completion_Task
+                      };
+                      TaskWindow taskWindow = new TaskWindow(vm);
+
+
+                      if (taskWindow.ShowDialog() == true)
+                      {
+                          // получаем измененный объект
+                          task = db.Tasks.Find(taskWindow.Task.Id_Task);
+                          if (task != null)
+                          {
+                              task.Name_Task = taskWindow.Task.Name_Task;
+                              task.Description_Task = taskWindow.Task.Description_Task;
+                              task.Performers_Task = taskWindow.Task.Performers_Task;
+                              task.Date_of_registration_Task = taskWindow.Task.Date_of_registration_Task;
+                              task.Status_Task = taskWindow.Task.Status_Task;
+                              task.Planned_time_Task = taskWindow.Task.Planned_time_Task;
+                              task.Actual_time_Task = taskWindow.Task.Actual_time_Task;
+                              task.Date_of_completion_Task = taskWindow.Task.Date_of_completion_Task;
+
+                              db.Entry(task).State = EntityState.Modified;
+                              db.SaveChanges();
+                          }
+                      }
+                  }));
+            }
+        }
+        // команда удаления
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ??
+                  (deleteCommand = new RelayCommand((selectedItem) =>
+                  {
+                      if (selectedItem == null) return;
+                      // получаем выделенный объект
+                      Task task = selectedItem as Task;
+                      db.Tasks.Remove(task);
+                      db.SaveChanges();
+                  }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
--- TMS_task_management_system/MainWindow.xaml.cs
+++ TMS_task_management_system/MainWindow.xaml.cs
@@ -32,74 +32,76 @@ namespace TMS_task_management_system
         {
             InitializeComponent();
 
-            db = new ApplicationContext();
-            db.Tasks.Load();
-            this.DataContext = db.Tasks.Local.ToBindingList();
+            this.DataContext = new ApplicationViewModel();
+
+            //db = new ApplicationContext();
+            //db.Tasks.Load();
+            //this.DataContext = db.Tasks.Local.ToBindingList();
         }
 
-        // добавление
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            TaskWindow taskWindow = new TaskWindow(new Task());
-            if (taskWindow.ShowDialog() == true)
-            {
-                Task task = taskWindow.Task;
-                db.Tasks.Add(task);
-                db.SaveChanges();
-            }
-        }
+        //// добавление
+        //private void Add_Click(object sender, RoutedEventArgs e)
+        //{
+        //    TaskWindow taskWindow = new TaskWindow(new Task());
+        //    if (taskWindow.ShowDialog() == true)
+        //    {
+        //        Task task = taskWindow.Task;
+        //        db.Tasks.Add(task);
+        //        db.SaveChanges();
+        //    }
+        //}
 
-        // редактирование
-        private void Edit_Click(object sender, RoutedEventArgs e)
-        {
-            // если ни одного объекта не выделено, выходим
-            if (tasksList.SelectedItem == null) return;
-            // получаем выделенный объект
-            Task task = tasksList.SelectedItem as Task;
+        //// редактирование
+        //private void Edit_Click(object sender, RoutedEventArgs e)
+        //{
+        //    // если ни одного объекта не выделено, выходим
+        //    if (tasksList.SelectedItem == null) return;
+        //    // получаем выделенный объект
+        //    Task task = tasksList.SelectedItem as Task;
 
-            TaskWindow taskWindow = new TaskWindow(new Task
-            {
-                Id_Task = task.Id_Task,
-                Name_Task = task.Name_Task,
-                Description_Task = task.Description_Task,
-                Performers_Task = task.Performers_Task,
-                Date_of_registration_Task = task.Date_of_registration_Task,
-                Status_Task = task.Status_Task,
-                Planned_time_Task = task.Planned_time_Task,
-                Actual_time_Task = task.Actual_time_Task,
-                Date_of_completion_Task = task.Date_of_completion_Task
-            });
+        //    TaskWindow taskWindow = new TaskWindow(new Task
+        //    {
+        //        Id_Task = task.Id_Task,
+        //        Name_Task = task.Name_Task,
+        //        Description_Task = task.Description_Task,
+        //        Performers_Task = task.Performers_Task,
+        //        Date_of_registration_Task = task.Date_of_registration_Task,
+        //        Status_Task = task.Status_Task,
+        //        Planned_time_Task = task.Planned_time_Task,
+        //        Actual_time_Task = task.Actual_time_Task,
+        //        Date_of_completion_Task = task.Date_of_completion_Task
+        //    });
 
-            if (taskWindow.ShowDialog() == true)
-            {
-                // получаем измененный объект
-                task = db.Tasks.Find(taskWindow.Task.Id_Task);
-                if (task != null)
-                {
-                    task.Name_Task = taskWindow.Task.Name_Task;
-                    task.Description_Task = taskWindow.Task.Description_Task;
-                    task.Performers_Task = taskWindow.Task.Performers_Task;
-                    task.Date_of_registration_Task = taskWindow.Task.Date_of_registration_Task;
-                    task.Status_Task = taskWindow.Task.Status_Task;
-                    task.Planned_time_Task = taskWindow.Task.Planned_time_Task;
-                    task.Actual_time_Task = taskWindow.Task.Actual_time_Task;
-                    task.Date_of_completion_Task = taskWindow.Task.Date_of_completion_Task;
+        //    if (taskWindow.ShowDialog() == true)
+        //    {
+        //        // получаем измененный объект
+        //        task = db.Tasks.Find(taskWindow.Task.Id_Task);
+        //        if (task != null)
+        //        {
+        //            task.Name_Task = taskWindow.Task.Name_Task;
+        //            task.Description_Task = taskWindow.Task.Description_Task;
+        //            task.Performers_Task = taskWindow.Task.Performers_Task;
+        //            task.Date_of_registration_Task = taskWindow.Task.Date_of_registration_Task;
+        //            task.Status_Task = taskWindow.Task.Status_Task;
+        //            task.Planned_time_Task = taskWindow.Task.Planned_time_Task;
+        //            task.Actual_time_Task = taskWindow.Task.Actual_time_Task;
+        //            task.Date_of_completion_Task = taskWindow.Task.Date_of_completion_Task;
 
-                    db.Entry(task).State = EntityState.Modified;
-                    db.SaveChanges();
-                }
-            }
-        }
-        // удаление
-        private void Delete_Click(object sender, RoutedEventArgs e)
-        {
-            // если ни одного объекта не выделено, выходим
-            if (tasksList.SelectedItem == null) return;
-            // получаем выделенный объект
-            Task task = tasksList.SelectedItem as Task;
-            db.Tasks.Remove(task);
-            db.SaveChanges();
-        }
+        //            db.Entry(task).State = EntityState.Modified;
+        //            db.SaveChanges();
+        //        }
+        //    }
+        //}
+        //// удаление
+        //private void Delete_Click(object sender, RoutedEventArgs e)
+        //{
+        //    // если ни одного объекта не выделено, выходим
+        //    if (tasksList.SelectedItem == null) return;
+        //    // получаем выделенный объект
+        //    Task task = tasksList.SelectedItem as Task;
+        //    db.Tasks.Remove(task);
+        //    db.SaveChanges();
+        //}
 
 
     }
